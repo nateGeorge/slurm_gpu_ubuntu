b77456f40b6243a7ced1f31bc52f963354745947
nateGeorge <wordsforthewise@gmail.com>
2019-08-14 21:16:33 -0600

    increase expire time and check if user exists

--- create_users.sh
+++ create_users.sh
@@ -7,9 +7,14 @@ defaultsalt="deepdream"
 ids=$(csvtool namedcol "User ID" $csvfile  | sed '1d')
 for id in $ids
 do
-  PASSWORD="$id$defaultsalt"
-  echo $PASSWORD
-  echo /storage/$id/
-  sudo useradd $id -d /storage/$id/ -m -g faculty -p $(openssl passwd -1 ${PASSWORD}) -e $(date '+%Y-%m-%d' -d '+1 year') -s /bin/#!/usr/bin/env bash
-  sudo sacctmgr -i create user name=$id account=students
+  if id "$id" > /dev/null 2>&1; then
+    # if user exists already, do nothing
+    :
+  else
+    PASSWORD="$id$defaultsalt"
+    echo $PASSWORD
+    echo /storage/$id/
+    sudo useradd $id -d /storage/$id/ -m -g faculty -p $(openssl passwd -1 ${PASSWORD}) -e $(date '+%Y-%m-%d' -d '+1 year +30 days') -s /bin/#!/usr/bin/env bash
+    sudo sacctmgr -i create user name=$id account=students
+  fi
 done
